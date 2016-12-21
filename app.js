fc8f1d446fd6ef13fd5d757c3bcd7fafd579fcde
root <root@server.puppet.com>
2016-12-21 15:36:01 +0530

    Added push 6 to master

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello welcome to jenkins push5');
+  res.send('hello welcome to jenkins push6');
 });
  
 app.listen(process.env.PORT || 5000);
